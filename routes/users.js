86b8bc844d9d06a3457601fd19ade3d17b793526
Kaoussi Souhail <kaoussi.souhail@gmail.com>
2018-12-02 22:09:56 +0100

    Refactoring

--- routes/users.js
+++ routes/users.js
@@ -1,30 +1,18 @@
 const passport = require('passport');
-
 const router = require('express-promise-router')();
-
 const UsersController = require('../controllers/users');
-
 const { validateBody, schemas } = require('../helpers/routeHelpers');
-
-const passportConfig = require('../passport');
+const passportAuth = passport.authenticate('local', { session: false });
+const passportJWT = passport.authenticate('jwt', { session: false });
 
 router
   .route('/signup')
   .post(validateBody(schemas.authSchema), UsersController.signUp);
 
-router.route('/signin').post(
-  validateBody(schemas.authSchema),
-  passport.authenticate('local', {
-    session: false
-  }),
-  UsersController.signIn
-);
-
 router
-  .route('/dashboard')
-  .get(
-    passport.authenticate('jwt', { session: false }),
-    UsersController.dashboard
-  );
+  .route('/signin')
+  .post(validateBody(schemas.authSchema), passportAuth, UsersController.signIn);
+
+router.route('/dashboard').get(passportJWT, UsersController.dashboard);
 
 module.exports = router;
