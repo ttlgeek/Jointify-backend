541b0fe300848d6a114da3cae69802ddf76141d7
Kaoussi Souhail <kaoussi.souhail@gmail.com>
2018-12-03 03:39:19 +0100

    importing passport config file

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const passport = require('passport');
 const router = require('express-promise-router')();
 const UsersController = require('../controllers/users');
+const passportConf = require('../passport');
 const { validateBody, schemas } = require('../helpers/routeHelpers');
 const passportAuth = passport.authenticate('local', { session: false });
 const passportJWT = passport.authenticate('jwt', { session: false });
