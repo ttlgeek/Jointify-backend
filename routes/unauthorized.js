ee62a87cff8743df7b9690d6dd5fa8cebc04d09c
Kaoussi Souhail <kaoussi.souhail@gmail.com>
2018-12-22 22:35:00 +0100

    unauthorized post route

--- routes/unauthorized.js
+++ routes/unauthorized.js
@@ -1,7 +1,15 @@
-const router = require("express-promise-router")();
+const router = require('express-promise-router')();
 
 router
-  .route("/unauthorized")
-  .get(async (req, res) => res.status(401).json({ message: "Unauthorized" }));
+  .route('/unauthorized')
+  .get(async (req, res) =>
+    res.status(401).json({ message: 'Unauthorized :D Sorry' })
+  );
+
+router
+  .route('/unauthorized')
+  .post(async (req, res) =>
+    res.status(401).json({ message: 'Unauthorized :D Sorry' })
+  );
 
 export default router;
--- routes/users.js
+++ routes/users.js
@@ -1,18 +1,18 @@
-import passport from "passport";
+import passport from 'passport';
 
-import { profile, signUp, signIn, dashboard } from "../controllers/users";
-import { validateBody, schemas } from "../helpers/routeHelpers";
-import { passportJWT, passportAuth } from "../config";
+import { profile, signUp, signIn, dashboard } from '../controllers/users';
+import { validateBody, schemas } from '../helpers/routeHelpers';
+import { passportJWT, passportAuth } from '../config';
 
-const router = require("express-promise-router")();
+const router = require('express-promise-router')();
 
-router.route("/signup").post(validateBody(schemas.authSchema), signUp);
+router.route('/signup').post(validateBody(schemas.authSchema), signUp);
 
 router
-  .route("/signin")
+  .route('/signin')
   .post(validateBody(schemas.authSchema), passportAuth, signIn);
 
-router.route("/dashboard").get(passportJWT, dashboard);
-router.route("/profile").get(passportJWT, profile);
+router.route('/dashboard').get(passportJWT, dashboard);
+router.route('/profile').get(passportJWT, profile);
 
 export default router;
